ddf261890bbd93f2104cb8f65a4f4269f6508288
DimoVanchev@gmail.com <DimoVanchev@gmail.com@69e677d5-2534-5495-d5df-986ad511ea32>
2012-02-27 20:07:32 +0000

    sandbox proj. move into yarar due to evakuatia

--- yarar/src/rikai/dunia/bio/LifeForm.java
+++ yarar/src/rikai/dunia/bio/LifeForm.java
@@ -1,46 +1,38 @@
 package rikai.dunia.bio;
 
-import rikai.kuptimor.Kuptimor;
+public class LifeForm // extends Kuptimor
+	implements Gender, LivingStatus {
 
-public class LifeForm extends Kuptimor implements Gender, LivingStatus {
-    
     protected boolean supernatural = false;
     protected int gender;
     protected int livingStatus;
-    
-    
+
     @Override
     public int getLivingStatus() {
 	return livingStatus;
     }
 
-
     @Override
-    public void setLivingStatus(int livingStatus) {
+    public void setLivingStatus(final int livingStatus) {
 	this.livingStatus = livingStatus;
     }
-
 
     @Override
     public int getGender() {
 	return gender;
     }
 
-
     @Override
-    public void setGender(int gender) {
+    public void setGender(final int gender) {
 	this.gender = gender;
     }
-
 
     protected boolean isSupernatural() {
 	return supernatural;
     }
-    
 
-    protected boolean setSupernatural(boolean supernatural) {
+    protected boolean setSupernatural(final boolean supernatural) {
 	return this.supernatural = supernatural;
     }
-    
 
 }
